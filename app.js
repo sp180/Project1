fe805bb51bc650cd4b14d7a842185ef833545c3a
User <speedhawk180@gmail.com>
2015-10-14 16:54:14 -0400

    created a readme markdown with project outline, added random wheel pick function

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 var wheelArray = ["100", "150", "200", "250", "300", "350", "400", "450", "500", "550", "600", "650", "750", "800", "850", "900", "950", "1000", "1050", "1100", "1150", "1200", "1250", "1300"];
 
+var randomWheelPick = wheelArray[Math.floor(Math.random()*wheelArray.length)];
 
+console.log(randomWheelPick);
 
 var playerOneScore = [""];
 var playerTwoScore = [""];
